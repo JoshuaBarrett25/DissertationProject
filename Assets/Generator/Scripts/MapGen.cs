411f9b9aa7bbe3247b061b57f0f68b4f69eba9b9
Joshua <joshua2.barrett@live.uwe.ac.uk>
2022-12-23 16:35:01 +0000

    Noise map creation with texture being applied to map

--- Assets/Generator/Scripts/MapGen.cs
+++ Assets/Generator/Scripts/MapGen.cs
@@ -5,21 +5,26 @@ using UnityEditor;
 
 public class MapGen : MonoBehaviour
 {
-    public Vector2 mapDimensions; public float mapNoiseScale;
+    public Vector2 dimensions; public float mapNoiseScale;
+    Renderer noiseRenderer;
 
+    //Generate TerrainObject. Will be the base for terrain generation
     public void GenPlane()
     {
         GameObject mapPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
         mapPlane.name = "MapTerrainPlane";
         mapPlane.tag = "Terrain";
         mapPlane.transform.position = new Vector3(0, 0, 0);
+        mapPlane.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Noise");
+        Material mat = Resources.Load<Material>("Materials/Noise");
+        noiseRenderer = mapPlane.GetComponent<Renderer>();
+        GenMap();
     }
 
     public void Export()
     {
         GameObject[] terrainGameObjects;
         terrainGameObjects = GameObject.FindGameObjectsWithTag("Terrain");
-        Debug.Log(terrainGameObjects[0]);
 
         if (terrainGameObjects != null)
         {
@@ -32,13 +37,16 @@ public class MapGen : MonoBehaviour
             for (int itr = 0; itr < terrainGameObjects.Length; itr++)
             {
                 PrefabUtility.SaveAsPrefabAsset(terrainGameObjects[itr], localPath);
+                Debug.Log("Model has been successfully exported: " + terrainGameObjects[itr].name);
             }
         }
     }
 
     public void GenMap()
     {
-        Vector2 generatedMapNoise = NoiseGen.GenNoiseMaps(mapNoiseScale, mapDimensions);
+        float[,] generatedMapNoise = NoiseGen.GenNoiseMaps(mapNoiseScale, (int)dimensions.x, (int)dimensions.y);
+        NoiseDisplay noiseDisplay = new NoiseDisplay();
+        noiseDisplay.RenderNoise(generatedMapNoise, noiseRenderer);
     }
 }
 
@@ -61,5 +69,30 @@ public class MapEditor : Editor
         {
             mapGen.Export();
         }
+    }
+}
+
+public class NoiseDisplay : MonoBehaviour
+{
+    public void RenderNoise(float[,] noise, Renderer noiseRenderer)
+    {
+        Vector2 dimensions;
+        dimensions.x = noise.GetLength(0);
+        dimensions.y = noise.GetLength(1);
+
+        Texture2D texture = new Texture2D((int)dimensions.x, (int)dimensions.y);
+
+        Color[] colour = new Color[(int)dimensions.x * (int)dimensions.y];
+        for (int y = 0; y < dimensions.y; y++)
+        {
+            for (int x = 0; x < dimensions.x; x++)
+            {
+                colour[y * (int)dimensions.x + x] = Color.Lerp(Color.black, Color.white, noise[x, y]);
+            }
+        }
+        texture.SetPixels(colour);
+        texture.Apply();
+        noiseRenderer.sharedMaterial.mainTexture = texture;
+        noiseRenderer.transform.localScale = new Vector3(dimensions.x, 1, dimensions.y);
     }
 }
--- Assets/Generator/Scripts/NoiseGen.cs
+++ Assets/Generator/Scripts/NoiseGen.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class NoiseGen : MonoBehaviour
 {
-    public static Vector2 GenNoiseMaps(float mapScale, Vector2 dimensions)
+    //Generates our noise map using the unity math function for perlin noise.
+    //Takes in parameters set by the user to scale the noise and place it on our terrain yielding different generations
+    public static float[,] GenNoiseMaps(float mapScale, int width, int height)
     {
-        Vector2 mapNoise;
-        mapNoise.x = dimensions.x;
-        mapNoise.y = dimensions.y;
+        float[,] mapNoise = new float[width,height];
 
-        //Robustness check
+        //Robustness check to make sure the scale is at least a value above 0 otherwise no noisemap will be present
         if (mapScale <= 0.0f)
         {
             mapScale = 0.001f;
         }
 
-        for (int y = 0; y < dimensions.y; y++)
+        //Scales our noise map along the dimensions the user has changed
+        for (int y = 0; y < height; y++)
         {
-            for (int x = 0; x < dimensions.x; x++)
+            for (int x = 0; x < width; x++)
             {
                 float scaledX = x / mapScale;
                 float scaledY = y / mapScale;
                 //Noise generated using scaled vectors
                 float noiseFactor = Mathf.PerlinNoise(scaledX, scaledY);
-                mapNoise.x = noiseFactor;
-                mapNoise.y = noiseFactor;
+                mapNoise[x, y] = noiseFactor;
             }
         }
         return mapNoise;
